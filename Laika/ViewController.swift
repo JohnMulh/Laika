9073cfccb2d2a41b74f8eaa6956b8913ddc24367
John Mulholland <jmulholland@John-iMac-2.local>
2014-11-06 17:25:23 +0000

    First working build

--- Laika/ViewController.swift
+++ Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogsAgeTextField: UITextField!
+    @IBOutlet weak var dogsAgeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let ageFromTextField = dogsAgeTextField.text.toInt()!
+        let conversionFactor = 7
+        dogsAgeLabel.text = "\(ageFromTextField * conversionFactor)"
+        dogsAgeLabel.hidden = false
+        dogsAgeTextField.text = ""
+        
+    }
+    
+    
 
 }
 
